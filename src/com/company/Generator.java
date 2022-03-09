94c86a42efff77ab867493e0c98509c9d84c438e
Kane Gin <kgin888@gmail.com>
2022-03-09 14:34:03 +0000

    Paired generator created

--- src/com/company/Generator.java
+++ src/com/company/Generator.java
@@ -7,61 +7,96 @@ import java.util.List;
 import java.util.Random;
 
 public class Generator {
-//    private static final List<Integer> LARGE_NUMBERS = new ArrayList<>(Arrays.asList(25,50,75,100));
+//    private static final List<int> LARGE_NUMBERS = new ArrayList<>(Arrays.asList(25,50,75,100));
     private static final Random random = new Random();
-//    private static final List<Integer> SMALL_NUMBERS = new ArrayList<>(Arrays.asList(21,2,3,4,5,6,7,8,9,10));
-// Approx 650ms for 100000
+    private static List<Integer> operators;
+//    private static final List<int> SMALL_NUMBERS = new ArrayList<>(Arrays.asList(21,2,3,4,5,6,7,8,9,10));
+// Approx 1900ms for 100000
     public static void main(String[] args) {
-//        long startTime = System.currentTimeMillis();
-//        for(int i=0; i<100000; i++) {
-//            Generator.generate(3);
-//        }
-//        long endTime = System.currentTimeMillis();
-//        System.out.println("without get: " + (endTime-startTime));
-        System.out.println(Generator.generate(3));
+        long startTime = System.currentTimeMillis();
+        for(int i=0; i<100000; i++) {
+            Generator.generate(3);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("without get: " + (endTime-startTime));
+//        System.out.println(Generator.generate(3));
     }
 
     public static String generate(int numberOfLarge) {
         long startTime = System.currentTimeMillis();
-        List<Integer> numbers = selectNumbers(numberOfLarge);
-
         while (true) {
-            int total = numbers.get(0);
-            int counter = 1;
-            String solution = numbers.get(0).toString();
-            while(counter < 6) {
-                int num = numbers.get(counter);
-                switch(random.nextInt(4)) {
-                    case 0:
-                        solution += " + " + num;
-                        total += num;
-                        counter++;
-                        break;
-                    case 1:
-                        solution += " * " + num;
-                        total *= num;
-                        counter++;
-                        break;
-                    case 2:
-                        solution += " - " + num;
-                        total -= num;
-                        counter++;
-                        break;
-                    case 3:
-                        if(total % num == 0) {
-                            solution += " / " + num;
-                            total /= num;
+            List<Integer> numbers = selectNumbers(numberOfLarge);
+            for(int j=0;j<10;j++) {
+                operators = new ArrayList<>();
+                // select operators
+                for (int i = 0; i < 5; i++) {
+                    if(i != 1) {
+                        operators.add(random.nextInt(2));
+                    } else {
+                        operators.add(random.nextInt(2)+2);
+                    }
+                }
+
+                int pair1 = getPair(0, numbers.get(0), numbers.get(1), operators.get(0));
+                int pair2 = getPair(2, numbers.get(2), numbers.get(3), operators.get(2));
+                List<Integer> pairedNumbers = Arrays.asList(pair1, pair2, numbers.get(4), numbers.get(5));
+                List<Integer> pairedOperators = new ArrayList<>(List.of(operators.get(1)));
+                pairedOperators.addAll(operators.subList(3,5));
+                int total = pair1;
+
+                for(int counter = 1; counter < 4;) {
+                    int num = pairedNumbers.get(counter);
+                    int operatorSwitch = pairedOperators.get(counter - 1);
+                    switch(operatorSwitch) {
+                        case 0:
+                            total += num;
                             counter++;
-                        }
-                        break;
+                            break;
+                        case 1:
+                            total -= num;
+                            counter++;
+                            break;
+                        case 2:
+                            total *= num;
+                            counter++;
+                            break;
+                        case 3:
+                            if(total % num == 0) {
+                                total /= num;
+                            } else {
+                                total *= num;
+                                operators.set(1, 2);
+                            }
+                            counter++;
+                            break;
+                    }
+                }
+                if(total >= 100 && total < 1000) {
+                    long endTime = System.currentTimeMillis();
+                    long duration = endTime-startTime;
+                    return "Numbers: " + numbers + "\nTotal: " + total + "\nOperators: " + operators + "\nDuration: " + duration + "ms";
                 }
             }
-            if(total >= 100 && total <= 1000) {
-                long endTime = System.currentTimeMillis();
-                long duration = endTime-startTime;
-                return "Numbers: " + numbers + "\nTotal: " + total + "\nSolution: " + solution + "\nDuration: " + duration + "ms";
-            }
+
+
         }
+    }
+
+    private static int getPair(int pair, int number1, int number2, int operator) {
+        int result;
+        if (operator == 1) {
+            int ans;
+            //todo check if assigning to ans faster than calcing twice
+            if ((ans = number1 - number2) > 0) {
+                result = ans;
+            } else {
+                result = number1 + number2;
+                operators.set(pair, 0);
+            }
+        } else {
+            result = number1 + number2;
+        }
+        return result;
     }
 
     private static List<Integer> selectNumbers(int numberOfLarge) {
